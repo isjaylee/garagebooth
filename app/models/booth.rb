ad591e775d60d2ed01250ea8cc4ebacb927d6219
isjaylee <leejay100@gmail.com>
2015-11-23 21:50:36 -0600

    Booth date fields now saving

--- app/models/booth.rb
+++ app/models/booth.rb
@@ -2,7 +2,7 @@ class Booth < ActiveRecord::Base
   belongs_to :user
   has_many :items
   has_many :images, as: :imageable
-  validates :name, presence: true
+  validates :name, :address1, :city, :state, :zipcode, :start_date, :stop_date, presence: true
 
   def other_items(booth, item)
     if booth.items.last(5).include?(item)
