4b71907f7c92ecd7d27f701a58bc7cdf20348ec1
isjaylee <leejay100@gmail.com>
2015-11-25 09:39:39 -0600

    Booth/Item edit functionality added

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,19 +1,33 @@
 class ItemsController < ApplicationController
+  before_action :find_booth, only: %i[new create show edit update]
 
   def new
-    @booth = Booth.find(params[:booth_id])
     @item = Item.new(booth: @booth)
   end
 
   def create
-    @booth = Booth.find(params[:booth_id])
     @item = @booth.items.build(item_params)
     @item.images.build(image: params[:item][:image][:image])
     if @item.save
-      redirect_to [@garage, @booth]
+      redirect_to [@booth, @item]
     else
       render :back
     end
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    @item.images.create(image: params[:item][:image][:image])
+    redirect_to [@booth, @item]
   end
 
   def search
@@ -24,15 +38,14 @@ class ItemsController < ApplicationController
     end
   end
 
-  def show
-    @booth = Booth.find(params[:booth_id])
-    @item = Item.find(params[:id])
-  end
-
   private
 
     def item_params
       params.require(:item).permit(:name, :description, :price, :category_id)
+    end
+
+    def find_booth
+      @booth = Booth.find(params[:booth_id])
     end
 
 end
