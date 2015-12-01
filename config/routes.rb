c3189e1819d38c74d44a57531a2cdb7191c627af
isjaylee <leejay100@gmail.com>
2015-12-01 06:40:23 -0600

    Cleaning up content

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   devise_for :users
+
   resources :booths do
     resources :items
   end
@@ -8,8 +9,8 @@ Rails.application.routes.draw do
   resources :categories
 
   get '/nearby_booths', to: "booths#nearby_booths", as: :nearby_booths
-  get '/dashboard', to: "users#dashboard", as: :dashboard
-  get '/items/search', to: "items#search", as: :items_search
-  post 'contact', to: 'messages#create'
+  get '/dashboard',     to: "users#dashboard",      as: :dashboard
+  get '/items/search',  to: "items#search",         as: :items_search
+  post 'contact',       to: 'messages#create'
   root "booths#index"
 end
